b90bdbefbd41c3ec0203e0718110837e7a891197
Kieston Guidry <kiestonkguidry@gmail.com>
2022-07-10 14:30:03 -0700

    pictures and stuff

---
+++ fun.js
@@ -0,0 +1,17 @@
+for (let i = 0; i < 5; i++) console.log("Hello World");
+
+// let i = 0;
+
+// while (i <= 5;) {
+//     if ( i % 2 !== 0) console.log(i);
+//     i++;
+// }
+
+// do-while
+
+let i = 0;
+
+do {
+  if (i % 2 !== 0) console.log(i);
+  i++;
+} while (i <= 5);
